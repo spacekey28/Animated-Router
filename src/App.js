d4eb7182acb057811e55e62cca2187975431f576
jazzed28 <peteson.me@gmail.com>
2018-03-20 14:14:48 +1300

    transition effect added with logo and css style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route } from 'react-router-dom';
 import React, { Component } from 'react';
 import Subscribe from './pages/subscribe';
 import ThankYou from './pages/thankyou';
+import PageShell from './components/pageshell';
+
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Route path="/" exact component={Subscribe}></Route>
-                <Route path="/thanks" exact component={ThankYou}></Route>
+                <Route path="/" exact component={PageShell(Subscribe)}></Route>
+                <Route path="/thanks" exact component={PageShell(ThankYou)}></Route>
             </div>
         );
     }
